824604864c85a8b9d90dac55fec1775dcea91448
Dmither <mr.d.graumann@gmail.com>
2024-06-25 08:30:02 +0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
-arr2 = np.array([[1, 2, 3], [4, 5, 6]])
-
-print(arr[0])
-print(arr2[1,1])
-
-arr3 = np.array([1, 2, 3], dtype='S')
-print(arr3, arr3.dtype)
+arrc = arr.copy()
+arrv = arr.view()
+arr[0] = 11
+arrc[1] = 22
+arrv[2] = 33
+print(arr)
+print(arrc)
+print(arrv)
+print(arrc.base)
+print(arrv.base)
